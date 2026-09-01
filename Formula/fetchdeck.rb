40c0085e300453e22f1819a2b6c6cae8829d257d
softmaxe <94467584+softmaxe@users.noreply.github.com>
2026-09-01 20:38:14 +0800

    chore: update FetchDeck repository URLs

--- Formula/fetchdeck.rb
+++ Formula/fetchdeck.rb
@@ -1,6 +1,6 @@
 class Fetchdeck < Formula
   desc "Friendly terminal interface for yt-dlp"
-  homepage "https://github.com/softmaxe/FetchDeck"
+  homepage "https://github.com/softmaxe/fetch-deck"
   license "MIT"
 
   depends_on "ffmpeg"
@@ -8,10 +8,10 @@ class Fetchdeck < Formula
   depends_on "yt-dlp"
 
   if Hardware::CPU.arm?
-    url "https://github.com/softmaxe/FetchDeck/releases/download/v1.0.1/fetchdeck-v1.0.1-aarch64-apple-darwin.tar.gz"
+    url "https://github.com/softmaxe/fetch-deck/releases/download/v1.0.1/fetchdeck-v1.0.1-aarch64-apple-darwin.tar.gz"
     sha256 "30c578c887d53ed368f26b702e7b5fb2c9bfed8f68ca7aa22293c18b9c6014f1"
   else
-    url "https://github.com/softmaxe/FetchDeck/releases/download/v1.0.1/fetchdeck-v1.0.1-x86_64-apple-darwin.tar.gz"
+    url "https://github.com/softmaxe/fetch-deck/releases/download/v1.0.1/fetchdeck-v1.0.1-x86_64-apple-darwin.tar.gz"
     sha256 "db1561620111e4b8fecb1a290292d3b626743e20c8ed5ae392deff9a31d22da9"
   end
 
